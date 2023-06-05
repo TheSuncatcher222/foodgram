4a91420480c35aff4ac0b1ffbe6411aeeb7dc218
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-05 15:33:00 +0300

    Register FootgramAppConfig

--- backend/footgram_backend/settings.py
+++ backend/footgram_backend/settings.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'footgram_app.apps.FootgramAppConfig',
 ]
 
 MIDDLEWARE = [
