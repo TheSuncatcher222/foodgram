47c708b27aa7a43a77d448222d19305148647c20
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-07 18:43:32 +0300

    Add CustomUserSerializer

---
+++ backend/api/v1/serializers.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from djoser.serializers import UserSerializer
+from rest_framework.serializers import SerializerMethodField
+
+from api.models import Subscriptions
+
+
+class CustomUserSerializer(UserSerializer):
+    """Переопределяет UserSerializer библиотеки Djoser.
+    Добавляет поле 'is_subscribed' в конец списка полей."""
+    is_subscribed = SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed')
+
+    def get_is_subscribed(self, obj):
+        """Показывает статус подписки пользователя в поле 'is_subscribed'.
+        Возвращает True, если пользователь имеет подписку, False - если не
+        имеет или пользователь не авторизован."""
+        if self.context['request'].user.is_anonymous:
+            return False
+        else:
+            return Subscriptions.objects.filter(
+                subscriber=self.context['request'].user,
+                subscription_to=obj).exists()
