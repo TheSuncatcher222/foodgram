5c1b3d3a07dfe69ff48523de0c6aea10c798a85a
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-07 23:49:09 +0300

    Complete "users" and "auth" paths

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from djoser.views import TokenCreateView, TokenDestroyView, UserViewSet
 from rest_framework.routers import DefaultRouter
 
 from api.v1.views import CustomUserViewSet
@@ -7,5 +8,14 @@ router = DefaultRouter()
 router.register(r'users', CustomUserViewSet, basename='users')
 
 urlpatterns = [
+    path('auth/token/login/',
+         TokenCreateView.as_view(),
+         name='token_create'),
+    path('auth/token/logout/',
+         TokenDestroyView.as_view(),
+         name='token_destroy'),
+    path('users/set_password/',
+         UserViewSet.as_view({'post': 'set_password'}),
+         name='set_password'),
     path('', include(router.urls)),
 ]
