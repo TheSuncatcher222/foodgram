6ab471626009e528b55dc75d01f9c53d6bc14131
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-09 19:08:18 +0300

    Register all models

--- backend/footgram_app/admin.py
+++ backend/footgram_app/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+from django.contrib.admin import site
 
-# Register your models here.
+from footgram_app.models import (
+    Ingredients, Recipes, RecipesFavoritesUsers, RecipesIngredients,
+    RecipesTags, ShoppingCarts, Subscriptions, Tags)
+
+site.register(Ingredients)
+site.register(Recipes)
+site.register(RecipesFavoritesUsers)
+site.register(RecipesIngredients)
+site.register(RecipesTags)
+site.register(ShoppingCarts)
+site.register(Subscriptions)
+site.register(Tags)
