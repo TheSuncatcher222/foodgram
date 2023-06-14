7b7ae838922a1e813097fbb0ba6ab65436aeeb85
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-14 16:53:04 +0300

    Add test_custom_user_serializer

---
+++ backend/api/v1/tests/test_serializers.py
@@ -0,0 +1,18 @@
+from rest_framework.serializers import (
+    CharField, EmailField, IntegerField, SerializerMethodField)
+
+from api.v1.serializers import CustomUserSerializer
+
+
+def test_custom_user_serializer():
+    expected_fields = {
+        'email': EmailField,
+        'id': IntegerField,
+        'username': CharField,
+        'first_name': CharField,
+        'last_name': CharField,
+        'is_subscribed': SerializerMethodField}
+    serializer = CustomUserSerializer()
+    assert list(serializer.fields.keys()) == list(expected_fields.keys())
+    for field, field_type in expected_fields.items():
+        assert isinstance(serializer.fields[field], field_type)
