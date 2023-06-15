f8f32edd9f1821efd96e0f103267d3cd4b1e9d27
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-15 17:23:43 +0300

    Add doc and add validate_username

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -1,9 +1,36 @@
+"""
+Создает сериализаторы моделей проекта "Footgram".
+
+Классы-сериализаторы:
+    - CustomUserSerializer (унаследован от UserSerializer)
+
+Создает ограничения для вводимых полей модели:
+    - USER_EMAIL_MAX_LEN - максимальная длина поля "email";
+    - USER_FIRST_NAME_MAX_LEN - максимальная длина поля "first_name";
+    - USER_PASSWORD_MAX_LEN - максимальная длина поля "password";
+    - USER_SECOND_NAME_MAX_LEN - максимальная длина поля "second_name";
+    - USER_USERNAME_MAX_LEN - максимальная длина поля "username";
+    - USER_FORBIDDEN_USERNAMES - список запрещенных значений поля "username";
+    - USERNAME_PATTERN - паттерн допустимых символов поля "username".
+"""
+
 from django.contrib.auth.models import User
 from djoser.serializers import UserSerializer
+from re import sub
 from rest_framework.serializers import (
     EmailField, CharField, SerializerMethodField, ValidationError)
 
 from footgram_app.models import Subscriptions
+
+USER_EMAIL_MAX_LEN: int = 254
+USER_FIRST_NAME_MAX_LEN: int = 150
+USER_PASSWORD_MAX_LEN: int = 150
+USER_SECOND_NAME_MAX_LEN: int = 150
+USER_USERNAME_MAX_LEN: int = 150
+
+USER_FORBIDDEN_USERNAMES: list[str] = ['me']
+
+USERNAME_PATTERN: str = r'^[\w.@+-]+$'
 
 
 class CustomUserSerializer(UserSerializer):
@@ -14,11 +41,12 @@ class CustomUserSerializer(UserSerializer):
             - 'first_name';
             - 'last_name';
         - производит валидацию на уникальность поля 'email'."""
-    email = EmailField()
+    email = EmailField(max_length=USER_EMAIL_MAX_LEN)
     is_subscribed = SerializerMethodField()
     password = CharField(
-        required=True,
+        max_length=USER_PASSWORD_MAX_LEN,
         write_only=True)
+    username = CharField(max_length=USER_USERNAME_MAX_LEN)
 
     class Meta:
         model = User
@@ -33,7 +61,8 @@ class CustomUserSerializer(UserSerializer):
         extra_kwargs = {
             'email': {'required': True},
             'first_name': {'required': True},
-            'last_name': {'required': True}}
+            'last_name': {'required': True},
+            'password': {'required': True}}
 
     def get_is_subscribed(self, obj):
         """Показывает статус подписки пользователя в поле 'is_subscribed'.
@@ -47,7 +76,31 @@ class CustomUserSerializer(UserSerializer):
                 subscription_to=obj).exists()
 
     def validate_email(self, value):
+        """Производит валидацию поля 'email':
+            - проверяет на уникальность."""
         if User.objects.filter(email=value).exists():
             raise ValidationError(
                 'Пользователь с такой электронной почтой уже существует.')
+        return value
+
+    def validate_username(self, value):
+        """Производит валидацию поля 'email':
+            - проверяет на уникальность;
+            - проверяет на несоответствие запрещенным именам."""
+        forbidden_chars: str = sub(
+            pattern=USERNAME_PATTERN,
+            repl='',
+            string=value)
+        if forbidden_chars:
+            sort_chars: str = ', '.join(
+                f'"{c}"' for c in sorted(set(forbidden_chars)))
+            raise ValidationError(
+                f'Использование {sort_chars} в имени пользователя запрещено.')
+        for forbidden in USER_FORBIDDEN_USERNAMES:
+            if value.lower() == forbidden.lower():
+                raise ValidationError(
+                    f'Пользователь с именем "{forbidden}" недопустим.')
+        if User.objects.filter(username=value).exists():
+            raise ValidationError(
+                'Пользователь с таким именем уже существует.')
         return value
