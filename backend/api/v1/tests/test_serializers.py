229e2256749719073b8744adb98887ad59544e97
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-17 16:56:43 +0300

    Add test_ingredients_serializer

--- backend/api/v1/tests/test_serializers.py
+++ backend/api/v1/tests/test_serializers.py
@@ -1,11 +1,12 @@
 from rest_framework.serializers import (
-    CharField, EmailField, IntegerField, SerializerMethodField)
+    CharField, ChoiceField, EmailField, IntegerField, SerializerMethodField)
 
-from api.v1.serializers import CustomUserSerializer
+from api.v1.serializers import CustomUserSerializer, IngredientsSerializer
 
 
-def test_custom_user_serializer():
-    expected_fields = {
+def test_custom_user_serializer() -> None:
+    """Тестирует поля сериализатора "CustomUserSerializer"."""
+    expected_fields: dict[str, any] = {
         'email': EmailField,
         'id': IntegerField,
         'username': CharField,
@@ -13,7 +14,19 @@ def test_custom_user_serializer():
         'last_name': CharField,
         'password': CharField,
         'is_subscribed': SerializerMethodField}
-    serializer = CustomUserSerializer()
+    serializer: CustomUserSerializer = CustomUserSerializer()
+    assert list(serializer.fields.keys()) == list(expected_fields.keys())
+    for field, field_type in expected_fields.items():
+        assert isinstance(serializer.fields[field], field_type)
+
+
+def test_ingredients_serializer() -> None:
+    """Тестирует поля сериализатора "IngredientsSerializer"."""
+    expected_fields = {
+        'id': IntegerField,
+        'name': CharField,
+        'measurement_unit': ChoiceField}
+    serializer: IngredientsSerializer = IngredientsSerializer()
     assert list(serializer.fields.keys()) == list(expected_fields.keys())
     for field, field_type in expected_fields.items():
         assert isinstance(serializer.fields[field], field_type)
