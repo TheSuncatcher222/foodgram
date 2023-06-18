0dd3d53e18e64e3b3355ce51b3c88a2d51ff1f06
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-18 15:35:54 +0300

    Add TagsViewSet

--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -5,14 +5,14 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from api.v1.serializers import CustomUserSerializer
-
+from api.v1.serializers import CustomUserSerializer, TagsSerializer
+from footgram_app.models import Tags
 
 class CustomUserViewSet(ModelViewSet):
     """
     Вью-сет обрабатывает следующие эндпоинты:
     1) `.../users/ - предоставляет информацию о пользователях при GET запросе,
-        создает нового пользователя при POST запросе.
+        создает нового пользователя при POST запросе;
     2) `.../users/pk/ - предоставляет информацию о пользователе с ID=pk
         при GET запросе.
     Дополнительные action-эндпоинты:
@@ -42,3 +42,15 @@ class CustomUserViewSet(ModelViewSet):
         """Добавляет action-эндпоинт `.../users/me/`."""
         serializer = self.get_serializer(request.user)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class TagsViewSet(ModelViewSet):
+    """
+    Вью-сет обрабатывает следующие эндпоинты:
+    1) `.../tags/ - предоставляет информацию о тегах при GET запросе;
+    2) `.../tags/{pk} - предоставляет информацию о теге с ID=pk
+       при GET запросе.
+    """
+    http_method_names = ('get', 'list')
+    serializer_class = TagsSerializer
+    queryset = Tags.objects.all()
