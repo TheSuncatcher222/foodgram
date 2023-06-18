75fabeff94b2b01100ab2a3883fa8dc77f35a4bd
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-18 12:21:34 +0300

    Add URL_UNAVALIABLE_STATUSES

--- backend/api/v1/tests/test_urls.py
+++ backend/api/v1/tests/test_urls.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APIClient
 
+URL_UNAVALIABLE_STATUSES: list = [
+    status.HTTP_301_MOVED_PERMANENTLY,
+    status.HTTP_302_FOUND,
+    status.HTTP_303_SEE_OTHER,
+    status.HTTP_307_TEMPORARY_REDIRECT,
+    status.HTTP_308_PERMANENT_REDIRECT,
+    status.HTTP_400_BAD_REQUEST,
+    status.HTTP_404_NOT_FOUND,
+    status.HTTP_408_REQUEST_TIMEOUT,
+    status.HTTP_409_CONFLICT,
+    status.HTTP_410_GONE]
+
 
 @pytest.fixture()
 def create_users() -> None:
@@ -30,9 +42,7 @@ class TestEndpointAvailability():
             - api/v1/auth/token/login/;
             - api/v1/auth/token/logout/."""
         response = self.client().get(f'/api/v1/auth/token/{url}')
-        assert response.status_code not in (
-            status.HTTP_301_MOVED_PERMANENTLY,
-            status.HTTP_404_NOT_FOUND)
+        assert response.status_code not in URL_UNAVALIABLE_STATUSES
 
     @pytest.mark.parametrize('url', ['', 'me/', '1/', 'set_password/'])
     def test_users_endpoint(self, url, create_users):
@@ -42,6 +52,4 @@ class TestEndpointAvailability():
             - api/v1/users/{pk}/;
             - api/v1/users/set_password/."""
         response = self.client().get(f'/api/v1/users/{url}')
-        assert response.status_code not in (
-            status.HTTP_301_MOVED_PERMANENTLY,
-            status.HTTP_404_NOT_FOUND)
+        assert response.status_code not in URL_UNAVALIABLE_STATUSES
