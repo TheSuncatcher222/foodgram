fb64be2f79d1aaf2d7234589195953ad3a3bfe11
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-18 11:46:50 +0300

    Add PageNumberPagination

--- backend/footgram_backend/settings.py
+++ backend/footgram_backend/settings.py
@@ -77,11 +77,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
+        'rest_framework.authentication.TokenAuthentication',],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ],
+        'rest_framework.permissions.AllowAny',],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
 }
 
 LANGUAGE_CODE = 'ru-ru'
