9cdb98c4ead57756f77ddabcd7a9b139c55cbead
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-21 15:05:02 +0300

    Add RecipesIngredientsCreateSerializer

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -22,12 +22,13 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.serializers import (
     ModelSerializer,
-    CharField, EmailField, FloatField, IntegerField, ListField, ReadOnlyField,
+    CharField, EmailField, IntegerField, ListField, PrimaryKeyRelatedField,
     SerializerMethodField,
+    SlugRelatedField,
     ValidationError)
 from footgram_app.models import (
-    Ingredients, Recipes, RecipesFavorites, RecipesIngredients,
-    RecipesTags, ShoppingCarts, Subscriptions, Tags)
+    Ingredients, Recipes, RecipesFavorites, RecipesIngredients, ShoppingCarts,
+    Subscriptions, Tags)
 
 USER_EMAIL_MAX_LEN: int = 254
 USER_FIRST_NAME_MAX_LEN: int = 150
@@ -132,7 +133,11 @@ class IngredientsSerializer(ModelSerializer):
 
 
 class RecipesIngredientsSerializer(ModelSerializer):
-    """Создает сериализатор для модели "RecipesIngredients"."""
+    """Создает сериализатор для поля "ingredients" в "RecipesSerializer"
+    для чтения объектов модели "RecipesIngredients" при обработке
+    HTTP-методов на чтение:
+        - "GET";
+        - "LIST"."""
 
     id = IntegerField(source='ingredient.id')
     name = CharField(source='ingredient.name')
@@ -151,13 +156,35 @@ class RecipesIngredientsSerializer(ModelSerializer):
         return fields
 
 
+class RecipesIngredientsCreateSerializer(ModelSerializer):
+    """Создает сериализатор для поля "ingredients" в "RecipesSerializer"
+    для создания объектов модели "RecipesIngredients" при обработке
+    HTTP-методов на запись:
+        - "PATCH";
+        - "POST";
+        - "PUT"."""
+
+    id = PrimaryKeyRelatedField(queryset=Ingredients.objects.all())
+    name = SerializerMethodField(read_only=True)
+    measurement_unit = SerializerMethodField(read_only=True)
+
+    class Meta():
+        model = RecipesIngredients
+        fields = ('id', 'name', 'measurement_unit', 'amount')
+
+    def get_name(self, obj):
+        """Получает значение поля "name" модели "ingredients"."""
+        return obj.ingredient.name
+
+    def get_measurement_unit(self, obj):
+        """Получает значение поля "measurement_unit" модели "ingredients"."""
+        return obj.ingredient.measurement_unit
+
+
 class RecipesSerializer(ModelSerializer):
     """Создает сериализатор для модели "Recipes"."""
 
     author = CustomUserSerializer(read_only=True)
-    ingredients = RecipesIngredientsSerializer(
-        many=True,
-        source='recipe_ingredient',)
     is_favorited = SerializerMethodField()
     is_in_shopping_cart = SerializerMethodField()
 
@@ -174,56 +201,22 @@ class RecipesSerializer(ModelSerializer):
             'image',
             'text',
             'cooking_time')
-        read_only_fields = ('author',)
 
-    # ToDo: сделать корректный код
-    # def create(self, validated_data):
-    #     """Переопределяет метод сохранения данных (POST)."""
-    #     validated_data['author'] = self.context['request'].user
-    #     ingredients: list = validated_data.get('ingredients', False)
-    #     tags: list = validated_data.get('tags', False)
-    #     recipe = Recipes.objects.create(**validated_data)
-    #     for ingredient in ingredients:
-    #         current_ingredient: Ingredients = (
-    #             Ingredients.objects.get(id=ingredient['id']))
-    #         recipe.recipe_ingredient.set(
-    #             amount=ingredient['amount'],
-    #             ingredient=current_ingredient)
-    #         RecipesIngredients.objects.create(
-    #             amount=ingredient['amount'],
-    #             recipe=recipe,
-    #             ingredient=current_ingredient)
-    #     for tag in tags:
-    #         current_tag = Tags.objects.get(id=tag)
-    #         recipe.recipe_tag.set(
-    #             recipe=recipe)
-    #     return recipe
-
-    # ToDo: сделать корректный код
-    # def update(self, instance, validated_data):
-    #     """Переопределяет метод обновления данных (PATCH)."""
-    #     instance.name = validated_data.get('name', instance.name)
-    #     instance.image = validated_data.get('image', instance.image)
-    #     instance.text = validated_data.get('text', instance.text)
-    #     instance.cooking_time = validated_data.get(
-    #         'cooking_time', instance.cooking_time)
-    #     ingredients = validated_data.get('ingredients')
-    #     if ingredients:
-    #         RecipesIngredients.objects.filter(recipe=instance).delete()
-    #         for ingredient in ingredients:
-    #             current_ingredient: Ingredients = (
-    #                 Ingredients.objects.get(id=ingredient['id']))
-    #             RecipesIngredients.objects.create(
-    #                 amount=ingredient['amount'],
-    #                 recipe=instance,
-    #                 ingredient=current_ingredient)
-    #     tags = validated_data.get('tags')
-    #     if tags:
-    #         RecipesTags.objects.filter(recipe=instance).delete()
-    #         for tag in tags:
-    #             current_tag = Tags.objects.get(id=tag)
-    #             RecipesTags.objects.create(recipe=instance, tag=current_tag)
-    #     return instance
+    def create(self, validated_data):
+        """Переопределяет метод сохранения данных (POST)."""
+        user: User = self.context['request'].user
+        ingredients_data: list[dict] = validated_data.pop('recipe_ingredient')
+        tags_data: list[int] = validated_data.pop('tags')
+        current_recipe: Recipes = Recipes.objects.create(
+            author=user, **validated_data)
+        for ingredient in ingredients_data:
+            current_amount: float = ingredient['amount']
+            RecipesIngredients.objects.create(
+                amount=current_amount,
+                ingredient=ingredient['id'],
+                recipe=current_recipe)
+        current_recipe.tags.set(tags_data)
+        return current_recipe
 
     def get_is_favorited(self, obj):
         """Показывает наличие рецепта в избранном пользователя в поле
@@ -248,8 +241,15 @@ class RecipesSerializer(ModelSerializer):
         от типа HTTP-запроса."""
         fields = super().get_fields()
         if self.context['request'].method in ('PATCH', 'POST', 'PUT'):
-            fields['tags'] = TagsIdListSerializer()
+            fields['ingredients'] = RecipesIngredientsCreateSerializer(
+                many=True,
+                source='recipe_ingredient')
+            fields['tags'] = SlugRelatedField(
+                many=True, queryset=Tags.objects.all(), slug_field="id")
         else:
+            fields['ingredients'] = RecipesIngredientsSerializer(
+                many=True,
+                source='recipe_ingredient')
             fields['tags'] = TagsSerializer(many=True)
         return fields
 
@@ -259,6 +259,10 @@ class RecipesSerializer(ModelSerializer):
     #         - добавляет пользователя из запроса в поле "author"."""
     #     serializer.save(author=self.request.user)
     #     return
+
+    # ToDo: добавить функцию, которая бы проверяла перед началом сохранения,
+    # что переданные значения ID тегов и ингредиентов существуют
+    # def validate(self, data: OrderedDict) -> OrderedDict:
 
 
 class RecipesFavoritesSerializer(ModelSerializer):
@@ -349,6 +353,7 @@ class ShoppingCartsSerializer(ModelSerializer):
 
 class TagsSerializer(ModelSerializer):
     """Создает сериализатор для модели "Tags"."""
+
     class Meta:
         model = Tags
         fields = (
