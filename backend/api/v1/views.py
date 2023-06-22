7ad7b940d1bbf80117f90514b525e6ad0596125e
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-22 19:40:07 +0300

    CustomUserViewSet: add subscriptions

--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -2,6 +2,7 @@ import csv
 
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -11,7 +12,8 @@ from rest_framework.viewsets import ModelViewSet
 from api.v1.permissions import IsAuthorOrAdminOrReadOnly
 from api.v1.serializers import (
     CustomUserSerializer, CustomUserSubscriptionsSerializer,
-    IngredientsSerializer, RecipesSerializer, TagsSerializer)
+    IngredientsSerializer, RecipesSerializer, SubscriptionsCreateSerializer,
+    SubscriptionsDeleteSerializer, TagsSerializer)
 from footgram_app.models import (
     Ingredients, Tags, Recipes, RecipesIngredients, ShoppingCarts,
     Subscriptions)
@@ -30,9 +32,15 @@ class CustomUserViewSet(ModelViewSet):
                            при GET запросе (доступно только
                            авторизированному пользователю).
     """
-    http_method_names = ('get', 'list', 'post')
+    http_method_names = ('get', 'list', 'post', 'delete')
     serializer_class = CustomUserSerializer
     queryset = User.objects.all()
+
+    def destroy(self, request, *args, **kwargs):
+        """Возвращает статус 405 при попытке совершения DELETE-запроса.
+        "Delete" включен явно в перечень разрешенных методов по причине того,
+        что он требуется для дочернего @action "subscribe"."""
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def get_queryset(self):
         """Обновляет метод передачи объектов модели в сериализатор:
@@ -49,7 +57,7 @@ class CustomUserViewSet(ModelViewSet):
             permission_classes=(IsAuthenticated,),
             serializer_class=CustomUserSerializer)
     def me(self, request):
-        """Добавляет action-эндпоинт `.../users/me/`."""
+        """Добавляет action-эндпоинт ".../users/me/"."""
         serializer = self.get_serializer(request.user)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
@@ -59,7 +67,7 @@ class CustomUserViewSet(ModelViewSet):
             permission_classes=(IsAuthenticated,),
             serializer_class=CustomUserSubscriptionsSerializer)
     def subscriptions(self, request):
-        """Добавляет action-эндпоинт `.../users/subscriptions/`, возвращающий
+        """Добавляет action-эндпоинт ".../users/subscriptions/", возвращающий
         пользователей, на которых подписан текущий пользователь. В выдачу
         добавляются рецепты."""
         subscriptions: Subscriptions = Subscriptions.objects.filter(
@@ -69,14 +77,48 @@ class CustomUserViewSet(ModelViewSet):
         serializer = self.get_serializer(users, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
+    @action(detail=False,
+            methods=('DELETE', 'POST'),
+            url_path=r'(?P<user_id>\d+)/subscribe',
+            permission_classes=(IsAuthenticated,))
+    def subscribe(self, request, user_id: int):
+        """Добавляет action-эндпоинт ".../users/{user_id}/subscribe/":
+            - POST: создает подсписку пользователя на автора с id=user_id;
+            - DELETE: удаляет подписку пользователя на автора с id=user_id."""
+        subscriber: User = request.user
+        subscription_to: User = get_object_or_404(User, id=user_id)
+        if request.method == 'DELETE':
+            serializer = SubscriptionsDeleteSerializer(
+                data={'subscriber': subscriber.id,
+                      'subscription_to': user_id})
+            serializer.is_valid(raise_exception=True)
+            Subscriptions.objects.get(
+                subscriber=subscriber,
+                subscription_to=subscription_to).delete()
+            data: None = None
+            status_code: status = status.HTTP_204_NO_CONTENT
+        elif request.method == 'POST':
+            serializer = SubscriptionsCreateSerializer(
+                data={
+                    'subscriber': subscriber.id,
+                    'subscription_to': user_id})
+            serializer.is_valid(raise_exception=True)
+            Subscriptions.objects.create(
+                subscriber=subscriber,
+                subscription_to=subscription_to)
+            serializer = CustomUserSubscriptionsSerializer(subscription_to)
+            data: dict = serializer.data
+            status_code: status = status.HTTP_200_OK
+        return Response(data=data, status=status_code)
+
 
 class IngredientsViewSet(ModelViewSet):
     """
     Вью-сет обрабатывает следующие эндпоинты:
-    1) ".../ingredients/"     - предоставляет информацию об ингредиентах
-                                при GET запросе;
-    2) ".../ingredients/{pk}" - предоставляет информацию об ингредиенте
-                                с ID=pk при GET запросе.
+    1) ".../ingredients/"      - предоставляет информацию об ингредиентах
+                                 при GET запросе;
+    2) ".../ingredients/{pk}/" - предоставляет информацию об ингредиенте
+                                 с ID=pk при GET запросе.
     """
     http_method_names = ('get', 'list')
     serializer_class = IngredientsSerializer
@@ -105,7 +147,7 @@ class RecipesViewSet(ModelViewSet):
             permission_classes=(IsAuthenticated,),
             serializer_class=CustomUserSerializer)
     def download_shopping_cart(self, request):
-        """Добавляет action-эндпоинт `.../recipes/download_shopping_cart/`.
+        """Добавляет action-эндпоинт ".../recipes/download_shopping_cart/".
         Формирует "shopping_cart.csv", который содержит в себе данные
         об ингредиентах рецептов в корзине пользователя со столбцами:
             - name: str, название ингредиента;
@@ -147,8 +189,8 @@ class RecipesViewSet(ModelViewSet):
 class TagsViewSet(ModelViewSet):
     """
     Вью-сет обрабатывает следующие эндпоинты:
-    1) `.../tags/      - предоставляет информацию о тегах при GET запросе;
-    2) `.../tags/{pk}/ - предоставляет информацию о теге с ID=pk
+    1) ".../tags/"      - предоставляет информацию о тегах при GET запросе;
+    2) ".../tags/{pk}/" - предоставляет информацию о теге с ID=pk
                          при GET запросе.
     """
     http_method_names = ('get', 'list')
