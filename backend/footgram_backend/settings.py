8260bde0089798dba6bf15438409f9296ff41420
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-27 01:47:09 +0300

    Register 'django_filters'

--- backend/footgram_backend/settings.py
+++ backend/footgram_backend/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
+    'django_filters',
     'api.apps.ApiConfig',
     'footgram_app.apps.FootgramAppConfig',
 ]
