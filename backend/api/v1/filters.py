848a184941ac4f1b2f71d1133e37fd498de8393e
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-27 01:50:15 +0300

    Add RecipesFilter

---
+++ backend/api/v1/filters.py
@@ -0,0 +1,75 @@
+from django_filters.rest_framework import (
+    Filter, FilterSet,
+    BooleanFilter, CharFilter)
+from rest_framework.exceptions import MethodNotAllowed
+
+from footgram_app.models import Recipes, RecipesFavorites, ShoppingCarts, User
+
+
+class TagsFilter(Filter):
+    """Вспомогательный фильтр для RecipesFilter, позволяющий вести фильтрацию
+    по Many-To-Many полю "tags" модели "Recipes"."""
+
+    def filter(self, queryset, value):
+        """Определяет список тегов, которые удовлетворяют введенному(ым)
+        значению(ям) поля "name" тега(ов)."""
+        if value:
+            """Согласно документации, при наличии списка тегов, они должны
+            быть разделены символом "_"."""
+            tags = value.split('_')
+            """Согласно документации, при нескольких указанных тегах должны
+            быть выданы те рецепты, у которых есть хотя бы один из тегов."""
+            queryset = queryset.filter(tags__slug__in=tags)
+        return queryset
+
+
+class RecipesFilter(FilterSet):
+    """Создает фильтр для "RecipesViewSet".
+    Позволяет осуществлять фильтрацию по полям:
+        - author;
+        - is_favorite: отображает только те рецепты, которые у пользователя
+                       добавлены в избранное (есть объект в RecipesFavorites);
+        - is_in_shopping_cart: отображает только те рецепты, которые у
+                               пользователя добавлены в корзину (есть объект
+                               в ShoppingCarts);
+        - tags: отображает только те рецепты, для которых определен(ы)
+                выбранный(е) тег(и) (через slug).
+    """
+    author = CharFilter(field_name='author__username')
+    is_favorite = BooleanFilter(method='filter_is_favorite')
+    is_in_shopping_cart = BooleanFilter(method='filter_is_in_shopping_cart')
+    tags = TagsFilter(field_name='tags__name')
+
+    class Meta:
+        model = Recipes
+        fields = ['author', 'is_favorite', 'tags']
+
+    def filter_is_favorite(self, queryset, name, value):
+        """Переопределяет queryset: фильтрует только те рецепты, которые
+        указаны в RecipesFavorites в паре с текущим пользователем."""
+
+        """Value - это переданное пользователем в запросе значение
+        исследуемого поля - "is_favorite"."""
+        if not value:
+            return queryset
+        user: User = self.request.user
+        if not user.is_authenticated:
+            raise MethodNotAllowed
+        recipe_ids: list = RecipesFavorites.objects.filter(
+            user=user).values_list('recipe_id', flat=True)
+        return Recipes.objects.filter(id__in=recipe_ids)
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        """Переопределяет queryset: фильтрует только те рецепты, которые
+        указаны в ShoppingCarts в паре с текущим пользователем."""
+
+        """Value - это переданное пользователем в запросе значение
+        исследуемого поля - "is_in_shopping_cart"."""
+        if not value:
+            return queryset
+        user: User = self.request.user
+        if not user.is_authenticated:
+            raise MethodNotAllowed
+        recipe_ids: list = ShoppingCarts.objects.filter(
+            user=user).values_list('recipe_id', flat=True)
+        return Recipes.objects.filter(id__in=recipe_ids)
