a43d99fe4562c2091f91aafc73589c3d902941b3
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-28 10:35:40 +0300

    Include api urlpatterns

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from api.urls import urlpatterns as api_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(api_urlpatterns)),
 ]
