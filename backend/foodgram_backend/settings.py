47825076d1899a85fd7db1ae7f48bd6c90a0a90c
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-29 21:41:55 +0300

    Add FOODGRAM_HOST

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'foodgram.ddns.net', '127.0.0.1']
+ALLOWED_HOSTS = ('localhost', '127.0.0.1', os.getenv('FOODGRAM_HOST'))
 
 INSTALLED_APPS = [
     'django.contrib.admin',
