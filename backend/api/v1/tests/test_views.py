3c96c2ed8741c1fa7244421b3932774d25441cfb
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-29 11:23:11 +0300

    test_ingredients_get: del pagination

--- backend/api/v1/tests/test_views.py
+++ backend/api/v1/tests/test_views.py
@@ -684,16 +684,12 @@ class TestIngredientsViewSet():
         """Тест GET-запроса списка ингредиентов по эндпоинту
         "/api/v1/ingredients/" для анонимного и авторизированного клиента.
         Используется фикстура "create_ingredients" для наполнения
-        тестовой БД ингредиентами.
-        В классе используется пагинация. В рамках теста производится анализ
-        содержимого "results" для первого элемента. Тест непосредственно
-        пагинации производится в функции "test_view_sets_pagination"."""
+        тестовой БД ингредиентами."""
         client: APIClient = client_func()
         response = client.get(URL_INGREDIENTS)
         assert response.status_code == status.HTTP_200_OK
         data: dict = json.loads(response.content)
-        results_pagination: dict = data['results']
-        assert results_pagination[0] == self.FIRST_INGREDIENT_EXP
+        assert data[0] == self.FIRST_INGREDIENT_EXP
         return
 
     @pytest.mark.parametrize('client_func', [anon_client, auth_client])
