859829df7b2c5c882a7cb9407e8c36f541d64ab7
Kirill Svidunovich <Altair21817@gmail.com>
2023-06-29 19:48:01 +0300

    Add Debug=True

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'foodgram.ddns.net', '127.0.0.1']
 
