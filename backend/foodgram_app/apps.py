8c17c210197abe2375d1445e37fe4eeeb7984c35
Kirill Svidunovich <Altair21817@gmail.com>
2023-07-18 12:53:51 +0300

    Add signals support

--- backend/foodgram_app/apps.py
+++ backend/foodgram_app/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class FoodgramAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'foodgram_app'
+
+    def ready(self):
+        from .signals import delete_recipe_ingredients, delete_recipe_tags
+        return
