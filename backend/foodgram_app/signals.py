00d7ca5967a0f3226dca594d941b3b681acee4d8
Kirill Svidunovich <Altair21817@gmail.com>
2023-07-18 12:54:08 +0300

    Add delete_recipe_ingredients

---
+++ backend/foodgram_app/signals.py
@@ -0,0 +1,25 @@
+import os
+
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+
+from .models import Ingredients, Recipes, RecipesIngredients, RecipesTags, Tags
+
+
+@receiver(signal=pre_delete, sender=Ingredients)
+def delete_recipe_ingredients(sender, instance, *args, **kwargs) -> None:
+    """При удалении объекта модели Ingredients также удаляет те объекты модели
+    Recipes, с которыми существует связь через RecipesIngredients.
+    Дополнительно уда"""
+    recipes_del_ids: list[int] = RecipesIngredients.objects.filter(
+        ingredient=instance).values_list('recipe_id', flat=True)
+    recipes_del: list[Recipes] = Recipes.objects.filter(id__in=recipes_del_ids)
+    media: list[str] = recipes_del.values_list('image', flat=True)
+    for path in media:
+        full_path: str = os.path.join('foodgram_app', 'media', path)
+        try:
+            os.remove(full_path)
+        except FileNotFoundError:
+            pass
+    recipes_del.delete()
+    return
