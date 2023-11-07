0b01a6407577c47d01d9fca887f6b3f9c6292965
Kirill Svidunovich <Altair21817@gmail.com>
2023-11-08 00:26:40 +0300

    Add TODO

--- backend/foodgram_app/apps.py
+++ backend/foodgram_app/apps.py
@@ -5,6 +5,7 @@ class FoodgramAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'foodgram_app'
 
-    def ready(self):
-        from .signals import delete_recipe_ingredients, delete_recipe_tags
-        return
+    # TODO: посмотреть сигналы
+    # def ready(self):
+    #     from .signals import delete_recipe_ingredients, delete_recipe_tags
+    #     pass
